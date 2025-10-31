63e1b9c21859434ec4fccdfcbf00748c57b39bed
Tourmaline28 <2022.relwin.esthak@ves.ac.in>
2025-10-31 20:17:59 +0530

    Updated project files

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,49 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDb  from './config/mongodb.js'
-import connectCloudinary from './config/cloudinary.js'
-import adminRouter from './routes/adminRoute.js'
-import doctorRouter from './routes/doctorRoute.js'
-import userRouter from './routes/userRoute.js'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDb from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import adminRouter from "./routes/adminRoute.js";
+import doctorRouter from "./routes/doctorRoute.js";
+import userRouter from "./routes/userRoute.js";
 
 // app config
-const app = express()
-const port = process.env.PORT || 4000
-connectDb()
-connectCloudinary()
+const app = express();
+const port = process.env.PORT || 4000;
+connectDb();
+connectCloudinary();
 
-//middlewares
-app.use(express.json())
-app.use(cors())
+// allowed origins
+const allowedOrigins = [
+  "http://localhost:5173", // frontend
+  "http://localhost:5174", // admin
+  "https://healthsoft.vercel.app",
+  "https://health-soft-admin.vercel.app",
+];
+
+// middlewares
+app.use(express.json());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true); // allow Postman or server-to-server requests
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"), false);
+      }
+    },
+    credentials: true,
+  })
+);
 
 // api endpoints
-app.use('/api/admin', adminRouter)
-app.use('/api/doctor', doctorRouter)
-app.use('/api/user', userRouter)
+app.use("/api/admin", adminRouter);
+app.use("/api/doctor", doctorRouter);
+app.use("/api/user", userRouter);
 
+app.get("/", (req, res) => {
+  res.send("API WORKING");
+});
 
-app.get('/', (req,res) => {
-    res.send('API WORKING')
-})
-
-app.listen(port, ()=> console.log("Server Started",port))
+app.listen(port, () => console.log("Server Started on port", port));
